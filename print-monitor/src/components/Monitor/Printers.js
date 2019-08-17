4c46c775f947786860212a93e85ca080836a1c4f
barros <barrospj@bc.edu>
2019-08-16 23:23:15 -0400

    add axios for http requests

--- print-monitor/src/components/Monitor/Printers.js
+++ print-monitor/src/components/Monitor/Printers.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Spinner } from 'reactstrap';
-import fetch from 'node-fetch';
+import axios from 'axios';
+import utils from '../../utils';
 
 class Printers extends React.Component {
   constructor(props) {
@@ -16,30 +17,7 @@ class Printers extends React.Component {
 
   fetchPrinters = () => {
     this.setState({ loading: true })
-    const PATH = `https://testpapercut.bc.edu/api/health/printers?Authorization=${process.env.REACT_APP_PAPERCUT_API_KEY}`
-    const headers = {
-      "Cache-Control": "no-cache",
-      "Accept": "application/json"
-    }
-    fetch(PATH, { method: 'GET', 
-                  headers: headers,
-                  mode: 'cors'})
-      .then((res) => {
-        return res.json()
-      })
-      .then((json) => {
-        this.printerJSONtoState(json)
-      })
-      .catch((err) => {
-        console.log("ERROR: "+err)
-        setTimeout(() => {
-          this.setState({ loading: false });
-        }, 5000);
-      })
-  }
-
-  printerJSONtoState = (data) => {
-    console.log(data)
+    // request the statuses from the backend
   }
 
   render() {
