7ad4a16ba533a26f5f3880d553b35e2b947fc02f
barros <barrospj@bc.edu>
2019-08-26 23:54:18 -0400

    create printer badge dictionary

--- print-monitor/src/utils.js
+++ print-monitor/src/utils.js
@@ -46,7 +46,32 @@ function getPrinterViewColor(dbStatus){
   return statuses[dbStatus];
 }
 
+function getBadge(dbStatus){
+  const statuses = {
+    'OK': 'ONLINE',
+    'PAPER_JAM': 'ATTENTION',
+    'PAPER_OUT': 'ATTENTION',
+    'PAPER_PROBLEM': 'ATTENTION',
+    'OUTPUT_BIN_FULL': 'ATTENTION',
+    'NOT_AVAILABLE': 'UNAVAILABLE',
+    'NO_TONER': 'ATTENTION',
+    'OUT_OF_MEMORY': 'ATTENTION',
+    'OFFLINE': 'OFFLINE',
+    'DOOR_OPEN': 'ATTENTION',
+    'USER_INTERVENTION': 'ATTENTION',
+    'ERROR': 'ERROR',
+    'UNKNOWN': 'UNKNOWN'
+  };
+
+  if (!statuses[dbStatus]){
+    return 'UNKNOWN';
+  }
+
+  return statuses[dbStatus];
+}
+
 module.exports = {
   getStatus: getStatus,
-  getPrinterViewColor: getPrinterViewColor
+  getPrinterViewColor: getPrinterViewColor,
+  getBadge: getBadge
 }
