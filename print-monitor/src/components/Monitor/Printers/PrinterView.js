ecec082b2e500090c112c27d22c2d3a052adc57c
barros <barrospj@bc.edu>
2019-08-27 23:16:44 -0400

    add status icon

--- print-monitor/src/components/Monitor/Printers/PrinterView.js
+++ print-monitor/src/components/Monitor/Printers/PrinterView.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Badge from './PrinterBadge';
 import Check from '@material-ui/icons/Check';
+import Help from '@material-ui/icons/Help';
+import Error from '@material-ui/icons/Error';
 import utils from '../../../utils';
 
 class PrinterView extends React.Component {
@@ -9,16 +11,33 @@ class PrinterView extends React.Component {
   }
 
   render(){
+    var iconJSX;
     const badgeText = utils.getBadge(this.props.printer.status);
+    var style = { 
+                  right:10,
+                  position: 'absolute', 
+                  fontSize: '35px', 
+                  color: utils.getIconColor(badgeText)
+                };
+    if (badgeText==='ONLINE'){
+      iconJSX = <Check style={style}/>
+    } else if (badgeText==='UNAVAILABLE' || badgeText==='UNKNOWN'){
+      iconJSX = <Help style={style}/>
+    } else {
+      iconJSX = <Error style={style}/>
+    }
 
     let body = utils.getStatus(this.props.printer.status);
-    let backgroundColor = utils.getPrinterViewColor(this.props.printer.status);
-    let fontColor = (backgroundColor==='#c50000' ? 'white' : 'black');
-    return (<div style={{position: 'relative', backgroundColor: '#f7f7f7', boxShadow: '2px 2px 15px #888888', width: '375px', height: 'auto', minHeight: '120px', marginBottom: '20px', paddingTop: '15px', paddingLeft: '15px', borderRadius:'10px'}}>
-              <Badge text={badgeText} backgroundColor={backgroundColor} fontColor={fontColor} />
-              <Check style={{right:10, position: 'absolute', fontSize: '35px', color: utils.getIconColor(badgeText)}}/>
-              <div style={{fontSize: '13pt', marginBottom: '10px'}}>{this.props.printer.name}</div>
-              <div style={{bottom:0, position: 'absolute', fontSize: '22pt'}}>{body}</div>
+    let backgroundColor = utils.getBadgeColor(badgeText);
+    let fontColor = ((backgroundColor==='#c50000' || backgroundColor==='#c46512') ? 'white' : 'black');
+    
+    return (<div style={{position: 'relative', backgroundColor: '#f7f7f7', boxShadow: '2px 2px 15px #888888', width: '375px', height: 'auto', minHeight: '120px', marginBottom: '20px', paddingTop: '13px', paddingLeft: '15px', borderRadius:'10px'}}>
+              <div style={{marginBottom: '5px'}}>
+                <Badge text={badgeText} backgroundColor={backgroundColor} fontColor={fontColor} />
+                {iconJSX}
+              </div>
+              <div style={{fontSize: '13pt'}}>{this.props.printer.name}</div>
+              <div style={{bottom: 0, right: 5, position: 'absolute', fontSize: '22pt'}}>{body}</div>
             </div>);
   }
 }
