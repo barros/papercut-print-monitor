1e889ff3647fb5e30b961fed7cfa2ec58b84906e
barros <barrospj@bc.edu>
2019-09-06 22:15:20 -0400

    add new loading spinner

--- print-monitor/src/components/Monitor/Printers/Printers.js
+++ print-monitor/src/components/Monitor/Printers/Printers.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Spinner } from 'reactstrap';
+import Loader from 'react-spinners/GridLoader';
 import PrinterView from './PrinterView';
 
 class Printers extends React.Component {
   render() {
-    let spinnerJSX;
     // List that will be updated with printer statuses
     let printerJSX = [];
     
     let locationDiv;
     if (this.props.selectedLocation){
       console.log(`location: ${this.props.selectedLocation}`)
-      locationDiv = <h2 style={{fontFamily: 'Montserrat'}}>{this.props.selectedLocation.name} Printers</h2>
+      locationDiv = <div>
+                      <h2 style={{fontFamily: 'Montserrat'}}>{this.props.selectedLocation.name} Printers</h2>
+                      <hr className="my-2" style={{paddingBottom:'25px'}}/>
+                    </div>;
     }
 
     if (this.props.printers){
@@ -19,18 +21,12 @@ class Printers extends React.Component {
         printerJSX.push(<PrinterView key={ printer._id } printer={ printer } style={{margin: 'auto', position: 'absolute'}} />);
       });
     }
-
-    if (this.props.loading) {
-      spinnerJSX = <Spinner color="info"/>
-    } else {
-      spinnerJSX = ""
-    }
+    
     return (
       <div style={{position: 'relative', marginTop: '10px', paddingBottom: '20px', marginLeft: '30px', marginRight: '30px', textAlign: 'center'}}>
         {locationDiv}
-        <hr className="my-2" style={{paddingBottom:'25px'}}/>
         <div>
-          {spinnerJSX}
+          <Loader css={'display: block; margin: 0 auto; border-color: red;'} color={'#a26864'} margin={'3px'} loading={this.props.loading} />
           <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', textAlign: 'left'}}>{printerJSX}</div>
         </div>
       </div>
