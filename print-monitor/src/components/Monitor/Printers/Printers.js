d04fc268bf2804788b9ec5c6c9fea22396cfc051
barros <barrospj@bc.edu>
2019-09-07 00:42:31 -0400

    add fade transition effect

--- print-monitor/src/components/Monitor/Printers/Printers.js
+++ print-monitor/src/components/Monitor/Printers/Printers.js
@@ -2,24 +2,25 @@ import React from 'react';
 import Loader from 'react-spinners/GridLoader';
 import PrinterView from './PrinterView';
 
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import '../../../Presentational/printers.css';
+
 class Printers extends React.Component {
   render() {
     // List that will be updated with printer statuses
-    let printerJSX = [];
+    let printerList = [];
+    if (this.props.printers){
+      this.props.printers.forEach(printer => {
+        printerList.push(<PrinterView key={ printer._id } printer={ printer } style={{margin: 'auto', position: 'absolute'}} />);
+      });
+    }
     
     let locationDiv;
     if (this.props.selectedLocation){
-      console.log(`location: ${this.props.selectedLocation}`)
       locationDiv = <div>
                       <h2 style={{fontFamily: 'Montserrat'}}>{this.props.selectedLocation.name} Printers</h2>
                       <hr className="my-2" style={{paddingBottom:'25px'}}/>
                     </div>;
-    }
-
-    if (this.props.printers){
-      this.props.printers.forEach(printer => {
-        printerJSX.push(<PrinterView key={ printer._id } printer={ printer } style={{margin: 'auto', position: 'absolute'}} />);
-      });
     }
     
     return (
@@ -27,7 +28,16 @@ class Printers extends React.Component {
         {locationDiv}
         <div>
           <Loader css={'display: block; margin: 0 auto; border-color: red;'} color={'#a26864'} margin={'3px'} loading={this.props.loading} />
-          <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', textAlign: 'left'}}>{printerJSX}</div>
+          <ReactCSSTransitionGroup
+            transitionName="fade"
+            transitionEnterTimeout={800}
+            transitionLeaveTimeout={50}
+            style={{ display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'space-around', 
+                    textAlign: 'left' }}>
+            {printerList}
+          </ReactCSSTransitionGroup>
         </div>
       </div>
     );
